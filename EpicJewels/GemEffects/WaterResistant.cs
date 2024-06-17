9842921a3a650252ff8a307c7f1e3035db98d532
Carl Stutz <carl.stutz@gmail.com>
2024-06-17 08:25:48 -0700

    replace landing page readme with symlink

--- EpicJewels/GemEffects/WaterResistant.cs
+++ EpicJewels/GemEffects/WaterResistant.cs
@@ -20,28 +20,33 @@ namespace EpicJewels.GemEffects
         }
 
         public static float delayWetTill = 0;
+        private static int wet_hash =  "Wet".GetHashCode();
 
         [HarmonyPatch(typeof(SEMan), nameof(SEMan.AddStatusEffect), typeof(int), typeof(bool), typeof(int), typeof(float))]
         public static class Waterproof_SEMan_AddStatusEffect_Patch
         {
             public static bool Prefix(SEMan __instance, int nameHash)
             {
-                if (__instance.m_character.IsPlayer() && nameHash == "Wet".GetHashCode())
+                if (__instance.m_character.IsPlayer() && nameHash == wet_hash)
                 {
                     if (Player.m_localPlayer.GetEffectPower<Config>("WaterResistant").Power > 0)
                     {
-                        if (Common.Config.EnableDebugMode.Value) { Jotunn.Logger.LogInfo($"Water Resistance activated."); }
+                        // if (Common.Config.EnableDebugMode.Value) { Jotunn.Logger.LogInfo($"Water Resistance activated."); }
                         if (delayWetTill == 0)
                         {
                             delayWetTill = Time.time + Player.m_localPlayer.GetEffectPower<Config>("WaterResistant").Power;
+                            if (Common.Config.EnableDebugMode.Value) { Jotunn.Logger.LogInfo($"Set water resistance timeout: {delayWetTill}"); }
+                            return false;
                         }
                         if (delayWetTill < Time.time)
                         {
+                            if (Common.Config.EnableDebugMode.Value) { Jotunn.Logger.LogInfo($"Water resistance is expired."); }
                             // if the delay wet timer is more than a few seconds past, we should reset it and avoid being wet right now
                             // this happens when the player tried getting wet, but then stopped having the wet effect applied before the resistance wore off
                             // eg: the delay wet timer is from an old application of wetness
                             if ((delayWetTill + 2) < Time.time)
                             {
+                                if (Common.Config.EnableDebugMode.Value) { Jotunn.Logger.LogInfo($"Reset water resistance."); }
                                 delayWetTill = 0;
                                 return false;
                             }
