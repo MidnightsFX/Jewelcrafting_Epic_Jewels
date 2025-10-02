a34f74769904218065033fdf283472227dd7720d
Styx <carl.stutz@gmail.com>
2025-10-01 22:07:20 -0700

    combat update

--- EpicJewels/GemEffects/StaggeringBlock.cs
+++ EpicJewels/GemEffects/StaggeringBlock.cs
@@ -17,15 +17,13 @@ namespace EpicJewels.GemEffects
         {
             private static void Postfix(Humanoid __instance, HitData hit, Character attacker, ref bool __result)
             {
-                if (__instance is Player player && player.GetEffectPower<Config>("Staggering Block").Power > 0 && __result == true && attacker.IsDead() == false)
-                {
+                if (__instance is Player player && player.GetEffectPower<Config>("Staggering Block").Power > 0 && __result == true && attacker != null && attacker.IsDead() == false) {
                     float roll = UnityEngine.Random.value;
                     float chance_max = (player.GetEffectPower<Config>("Staggering Block").Power / 100);
                     // EpicJewels.EJLog.LogDebug($"Staggering block chance roll: {roll} < {chance_max}");
-                    if (roll < chance_max)
-                    {
+                    if (roll < chance_max) {
                         // EpicJewels.EJLog.LogDebug($"Staggering attacker");
-                        attacker.AddStaggerDamage(999f, -hit.m_dir);
+                        attacker.AddStaggerDamage(999f, -hit.m_dir, null);
                     }  
                 }
             }
